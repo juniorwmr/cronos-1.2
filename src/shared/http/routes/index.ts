263aeefe80bd2b02d4849a37ccaf1af4b495c871
Washington Muniz <juniorripardo@gmail.com>
2021-11-21 19:22:11 -0500

    ♻️ BVP-000 Testing

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import { employeesRoutes } from './employees.routes';
 import { adminsRoutes } from './admin.routes';
 const routes = Router();
 
+routes.get('/', (request, response) => response.send('Hello World!'));
 routes.use('/admin', adminsRoutes);
 routes.use('/employee', employeesRoutes);
 
